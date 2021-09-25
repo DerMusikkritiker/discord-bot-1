3c86882cdb577bc3405fcb66b07694faf56269aa
Jakob Adomeit <jakob.k.adomeit@web.de>
2021-09-25 14:26:36 +0100

    import dotenv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const { Client, Intents } = require("discord.js")
 
 // specifying intents
@@ -5,6 +6,7 @@ const setup_intents = new Intents()
 setup_intents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES)
 const client = new Client( {intents: setup_intents} )
 
+// actions
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
@@ -15,4 +17,4 @@ client.on("message", msg => {
     }
 })
 
-client.login("ODkxMjgwOTQwMDcwOTkzOTUw.YU8EDw.jEd2AmGPO3j138vFSQ_cey5-0pQ")
+client.login(process.env.DISCORDJS_BOT_TOKEN)
