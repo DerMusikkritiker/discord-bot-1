496251ac73a522e6d84a467f3e83dca415517825
Jakob Adomeit <jakob.k.adomeit@web.de>
2021-09-25 15:20:18 +0100

    tried some stuff, not working yet importing node-fetch is a problem

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 require("dotenv").config()
 const { Client, Intents } = require("discord.js")
+import fetch from 'node-fetch'
 
 // specifying intents
 const setup_intents = new Intents()
 setup_intents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES)
 const client = new Client( {intents: setup_intents} )
 
-// actions
+// Console updates
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
+// messages
 client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("peng")
+    if (msg.author.bot) return
+    
+    if (msg.content === "numberfact") {
+        getRandomFact(null).then(quote => msg.channel.send(quote))
+    }
+
+    if (msg.content === "Hello") {
+        msg.reply("Hello there!")
     }
 })
 
+// random number fact
+function getRandomFact(randomNumber) {
+    if (randomNumber != null) {
+        request = randomNumber
+    } else {
+        request = "random/trivia"
+    }
+    return fetch("http://numbersapi.com/" + request)
+    .then(res => {
+        return res.json()
+    })
+}
+
+// logging in
 client.login(process.env.DISCORDJS_BOT_TOKEN)
